4c8828a398c7a7be79e97aad8633a44a9ba73974
wshunli <wshunli@qq.com>
2017-12-16 20:59:36 +0800

    feature:集成 React Native

---
+++ app/src/main/java/com/wshunli/rnweibo/App.java
@@ -0,0 +1,38 @@
+package com.wshunli.rnweibo;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Created by wshunli on 2017/12/16.
+ */
+
+public class App extends Application implements ReactApplication {
+
+    private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+        @Override
+        public boolean getUseDeveloperSupport() {
+            return BuildConfig.DEBUG;
+        }
+
+        @Override
+        protected List<ReactPackage> getPackages() {
+            return Arrays.<ReactPackage>asList(
+                    new MainReactPackage()
+
+
+            );
+        }
+    };
+    @Override
+    public ReactNativeHost getReactNativeHost() {
+        return mReactNativeHost;
+    }
+}
---
+++ index.android.js
@@ -0,0 +1,32 @@
+'use strict';
+
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+
+class WeiboHTML5 extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.hello}>Hello, React Native</Text>
+      </View>
+    )
+  }
+}
+var styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+  },
+  hello: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+  },
+});
+
+AppRegistry.registerComponent('WeiboHTML5', () => WeiboHTML5);
